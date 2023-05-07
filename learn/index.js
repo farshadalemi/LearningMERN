8d43d0bf80eb415d51bc7e71ea40f79146c6c78e
Farshad <farshad.alemi@gmail.com>
2023-05-08 01:54:24 +0330

    Read File Sychronously & Accomplished API

--- learn/index.js
+++ learn/index.js
@@ -3,6 +3,10 @@ const http = require("http");
 const url = require("url");
 
 // SERVER
+//// We read the file only one time Synchronously to avoid rereading the same data evrytime.
+
+const data = fs.readFileSync(`${__dirname}/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
   // console.log(req);
@@ -13,12 +17,15 @@ const server = http.createServer((req, res) => {
     res.end("This is the overview page");
   } else if (pathName === "/product") {
     res.end("This is the product page");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(data);
   } else {
     res.writeHead(404, {
       "Content-type": "text/html",
       "My-own-header": "Hello, world",
     });
-    res.end('<h1>Page not found!</h1>');
+    res.end("<h1>Page not found!</h1>");
   }
 });
 
--- learn/learn.js
+++ learn/learn.js
@@ -14,7 +14,7 @@
 // fs.writeFileSync('learn.txt', nestIntroTitle);
 // console.log('file written');
 
-// /* Why it will rewrite over the file? Answer: ** To avoid rewriting, we need to  utilize "fs.appendFileSync" 
+// /* Why it will rewrite over the file? Answer: ** To avoid rewriting, we need to  utilize "fs.appendFileSync"
 
 // const Aouthor = "Farshad";
 // const nestIntroTitle = `Introduction:\n ${nestIntro}\nCreated on: ${Date.now()}\nWritten by ${Aouthor}`;
@@ -23,11 +23,11 @@
 // console.log("file written");
 
 // /*Non-Blocking, Asynchronous Way*/
-const fs = require('fs');
+const fs = require("fs");
 
 // //1
 // fs.readFile('learn.txt', 'utf-8' ,(err, data) => {
-    // console.log(data);
+// console.log(data);
 // });
 // console.log("*** >>>>>> First read this file");
 
@@ -40,28 +40,23 @@ const fs = require('fs');
 // })
 
 // //Call-back hell
-fs.readFile('question.txt', 'utf-8', (err, data1)=>{
-    if (err) return console.log("Error");
+fs.readFile("question.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("Error");
 
-    fs.readFile(`${data1}.txt`, 'utf-8', (err, data2)=>{
-        console.log(data1);
-        fs.readFile('answer.txt', 'utf-8', (err, data3)=>{
-            console.log(data3);
-            fs.appendFile('data.txt', `\n${data1}\n${data3}` ,'utf-8', err => {
-                console.log("Your file has been successfully written.");
-            });
-        });
+  fs.readFile(`${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data1);
+    fs.readFile("answer.txt", "utf-8", (err, data3) => {
+      console.log(data3);
+      fs.appendFile("data.txt", `\n${data1}\n${data3}`, "utf-8", (err) => {
+        console.log("Your file has been successfully written.");
+      });
     });
+  });
 });
-
 
 // //2
-const newMessage = "\n\n I would master Node.js within a few next weeks." 
-fs.appendFile('learn.txt', newMessage ,(err) => {
-    if (err)
-        throw err;
-    console.log("Message has been appended successfully.");
+const newMessage = "\n\n I would master Node.js within a few next weeks.";
+fs.appendFile("learn.txt", newMessage, (err) => {
+  if (err) throw err;
+  console.log("Message has been appended successfully.");
 });
-  
-
- 
